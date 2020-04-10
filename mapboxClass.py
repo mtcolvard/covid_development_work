63a6001be85653943728f4eb1a75d6d26725e331
mtcolvard <matthew.colvard@gmail.com>
2020-04-10 22:28:44 +0200

    reasessing mapbox api sdk instructions

--- mapboxClass.py
+++ mapboxClass.py
@@ -1,25 +1,7 @@
-from mapbox import Directions
+from mapbox import DirectionsMatrix
 
-# class mapbox.services.matrix.DirectionsMatrix(access_token=None, host=None, cache=None)
-# Bases: mapbox.services.base.Service
-#
-# Access to the Matrix API V1
-#
-# Attributes:
-# baseuri
-# The service’s base URI
-#
-# username
-# The username in the service’s access token
 origin = [17.06009, 51.13386]
 
-
-class DirectionsMatrix:
-    def __init__(self, name):
-        self.name = name
-
-
-    def matrix(self, coordinates, profile='mapbox/driving', sources=None, destinations=None, annotations=None):
-        c
+service = DirectionsMatrix()
 
 # def main():
