cf10e382d4febdcb3ec26cee483c9d655e2e9a9f
mtcolvard <matthew.colvard@gmail.com>
2020-04-12 20:35:51 +0200

    working on __geo_interface__ implimentation from the geojson python library

--- mapboxClass.py
+++ mapboxClass.py
@@ -27,18 +27,24 @@ Woodside_Park  =  {'type': 'Feature', 'properties': {'name': 'Woodside Park'}, '
 origin = [-0.071132, 51.518891]
 destination = [-0.091250, 51.505262]
 
+waypoint_coordinates = r'[-0.048721755995117,51.51258512161],
+[-0.045418257473052,51.522421981895],
+[-0.01279619151928,51.510183314858],
+[-0.068770393713962,51.515614772134],
+[-0.045302810969366,51.525117839052],
+[-0.055348551291013,51.526184094351],
+[-0.075555186872678,51.525618133595],
+[-0.017776262243614,51.528252888321]'
 
 service = DirectionsMatrix(access_token='pk.eyJ1IjoibXRjb2x2YXJkIiwiYSI6ImNrMDgzYndkZjBoanUzb21jaTkzajZjNWEifQ.ocEzAm8Y7a6im_FVc92HjQ')
 
-response = service.matrix([origin, destination, York_Terrace_West, York_Square_Gardens, York_Rise_Estate_Gardens, York_Gardens, Yeading_Walk], profile='mapbox/walking', sources=[0, 1], annotations=['distance'])
+response = service.matrix([origin, destination, [-0.048721755995117,51.51258512161]], profile='mapbox/walking', sources=[0, 1], annotations=['distance'])
 
-
-response.headers['Content-Type']
-'application/json; charset=utf-8'
+# York_Terrace_West, York_Square_Gardens, York_Rise_Estate_Gardens, York_Gardens, Yeading_Walk
 
 data = response.json()
-statusCode = response.status_code
-header = response.headers
+responseStatusCode = response.status_code
+responseHeader = response.headers
 
 def find_route_waypoints():
     # calculate the distance to each possible waypoint from both the origin and the destination
@@ -59,11 +65,44 @@ find_route_waypoints()
 
 #
 from pprint import pprint
-# pprint(statusCode)
+# pprint(responseStatusCode)
+pprint(responseHeader)
 # pprint(response.json()['distances'][0])
 # pprint(distances_from_origin)
 
-
-
-
 # def main():
+
+# [-0.061301863303676,51.521786360354],
+# [-0.013532569137843,51.526382561487],
+# [-0.05720990921333,51.516323089494],
+# [-0.051065100867099,51.525213734392],
+# [-0.019177902823842,51.529175831881],
+# [-0.035102821248716,51.527645181073],
+# [-0.066721235716513,51.529969137313],
+# [-0.050315406539387,51.509014581232],
+# [-0.041366715819842,51.516059558877],
+# [-0.045225836659679,51.526915076361],
+# [-0.041443812107358,51.51426232236],
+# [-0.055616524858349,51.519893722332],
+# [-0.010855329562293,51.488567979003],
+# [-0.055271969554349,51.527981342069],
+# [-0.056712637941541,51.528005220953],
+# [-0.017116408507303,51.510256445333],
+# [-0.041096815873977,51.522349881668],
+# [-0.032918650586594,51.511421844157],
+# [-0.039579150441387,51.524123045013],
+# [-0.045418257473052,51.522421981895],
+# [-0.041212499269476,51.519654029999],
+# [-0.03147852577133,51.511397678601],
+# [-0.042806996167133,51.516083604578],
+# [-0.060395909406252,51.509181744854],
+# [-0.041597980712523,51.510667847417],
+# [-0.062064781654964,51.503813771304],
+# [-0.013454340529617,51.528179764083],
+# [-0.064335241794335,51.518239429505],
+# [-0.017155426711002,51.509357839359],
+# [-0.053195540296469,51.509062430618],
+# [-0.065280496813107,51.529945380685],
+# [-0.044208783987261,51.517006252163],
+# [-0.048452902742563,51.518875475744],
+# [-0.047050971377665,51.51795287766],
--- mapboxGeoJsonConstructor.py
+++ mapboxGeoJsonConstructor.py
@@ -2,27 +2,90 @@
 # import pydash
 
 origin = [-0.071132, 51.518891]
-collection_one = {'York Terrace West':[-0.150578775,51.52412738]
-,
-'York Square Gardens':[-0.040042164,51.51333964],
-'York Rise Estate Gardens':[-0.143500164,51.55638842],
-'York House Gardens *':[-0.323488655,51.44759367],
-'York Gardens':[-0.177365387,51.46699476],
-'Yeading Walk':[-0.367516667,51.58489741],
-'Yardley Recreation Ground':[-0.228778185,51.36616707],
-'Yalta Memorial Garden':[-0.17481463,51.49483187],
-'Wyck Gardens':[-0.105685898,51.4595636],
-'WWT London Wetland Centre':[-0.236013033,51.47688433],
-'Wrythe Recreation Ground':[-0.174007434,51.37072169],
-'Wrythe Green':[-0.166756541,51.37240698],
-'Wormwood Scrubs including Old Oak Common':[-0.235717844,51.52184199],
-'Wormholt Park':[-0.239049728,51.51020195],
-'Wormholt Estate':[-0.243301365,51.51206419],
-'World\'s End Estate':[-0.17845059,51.48131873],
-'Woolwich Common and Royal Artillery Barracks':[0.053470899,51.47756693],
-'Woolwich Cemetery (Old and New)':[0.096639447,51.47680282],
-'Wool House Garden':[-0.135466095,51.50590308],
-'Woodside Park':[-0.111252444,51.60353366]
+collection_one = {
+"Albert Gardens"	:(-0.048721755995117,51.51258512161),
+
+"Alderney Road Cemetery"	:(-0.045418257473052,51.522421981895),
+
+"All Saints Churchyard, Poplar"	:(-0.01279619151928,51.510183314858),
+
+"Altab Ali Park"	:(-0.068770393713962,51.515614772134),
+
+"Bancroft Road Cemetery"	:(-0.045302810969366,51.525117839052),
+
+"Bethnal Green Gardens"	:(-0.055348551291013,51.526184094351),
+
+"Boundary Gardens *"	:(-0.075555186872678,51.525618133595),
+
+"Bow Churchyard"	:(-0.017776262243614,51.528252888321),
+
+"Brady Street Cemetery"	:(-0.061301863303676,51.521786360354),
+
+"Bromley Recreation Ground"	:(-0.013532569137843,51.526382561487),
+
+"Ford Square Gardens"	:(-0.05720990921333,51.516323089494),
+
+"Globe Road Open Space"	:(-0.051065100867099,51.525213734392),
+
+"Grove Hall Park"	:(-0.019177902823842,51.529175831881),
+
+"Holy Trinity Church Graveyard"	:(-0.035102821248716,51.527645181073),
+
+"Ion Square Gardens"	:(-0.066721235716513,51.529969137313),
+
+"King Edward Memorial Park"	:(-0.050315406539387,51.509014581232),
+
+"Lady Jane Mico's Almshouses"	:(-0.041366715819842,51.516059558877),
+
+"Meath Gardens"	:(-0.045225836659679,51.526915076361),
+
+"Mercers Burial Ground"	:(-0.041443812107358,51.51426232236
+),
+"Mile End Road Verges"	:(-0.055616524858349,51.519893722332),
+
+"Millwall Park"	:(-0.010855329562293,51.488567979003),
+
+"Museum Gardens *"	:(-0.055271969554349,51.527981342069),
+
+"Paradise Gardens *"	:(-0.056712637941541,51.528005220953),
+
+"Poplar Park"	:(-0.017116408507303,51.510256445333),
+
+"Queen's Building Forecourt, Queen Mary, University of London"	:(-0.041096815873977,51.522349881668),
+
+"Rectory Gardens"	:(-0.032918650586594,51.511421844157),
+
+"Sephardi Nuevo (New) Cemetery *"	:(-0.039579150441387,51.524123045013),
+
+"Sephardi Velho (Old) Cemetery"	:(-0.045418257473052,51.522421981895),
+
+"Shandy Park"	:(-0.041212499269476,51.519654029999),
+
+"St Anne's Churchyard, Limehouse"	:(-0.03147852577133,51.511397678601),
+
+"St Dunstan's Churchyard, Stepney"	:(-0.042806996167133,51.516083604578),
+
+"St George's-in-the-East Churchyard"	:(-0.060395909406252,51.509181744854),
+
+"St James's Gardens"	:(-0.041597980712523,51.510667847417),
+
+"St John's Gardens"	:(-0.062064781654964,51.503813771304),
+
+"St Leonardâ€™s Priory"	:(-0.013454340529617,51.528179764083),
+
+"St Mary's Additional Burial Ground"	:(-0.064335241794335,51.518239429505),
+
+"St Matthias Churchyard"	:(-0.017155426711002,51.509357839359),
+
+"St Paul's Churchyard, Shadwell"	:(-0.053195540296469,51.509062430618),
+
+"St Peter's Churchyard"	:(-0.065280496813107,51.529945380685),
+
+"Stepney City Farm and Rural Arts Centre"	:(-0.044208783987261,51.517006252163),
+
+"Stepney Green Gardens"	:(-0.048452902742563,51.518875475744),
+
+"Stepney Green Park and Stepney Clocktower"	:(-0.047050971377665,51.517952877664)
 }
 
 class ConstructGeoJson:
@@ -45,7 +108,7 @@ def create_list_names():
     #     print(list_names)
     # x = collection_one.keys()
     # print(x)
-    y = r'York Terrace West', 'York Square Gardens', 'York Rise Estate Gardens', 'York House Gardens *', 'York Gardens', 'Yeading Walk', 'Yardley Recreation Ground', 'Yalta Memorial Garden', 'Wyck Gardens', 'WWT London Wetland Centre', 'Wrythe Recreation Ground', 'Wrythe Green', 'Wormwood Scrubs including Old Oak Common', 'Wormholt Park', 'Wormholt Estate', "World's End Estate", 'Woolwich Common and Royal Artillery Barracks', 'Woolwich Cemetery (Old and New)', 'Wool House Garden', 'Woodside Park'
+    York Terrace West, York Square Gardens, York Rise Estate Gardens, York House Gardens *, York Gardens, Yeading Walk, Yardley Recreation Ground, Yalta Memorial Garden, Wyck Gardens, WWT London Wetland Centre, Wrythe Recreation Ground, Wrythe Green, Wormwood Scrubs including Old Oak Common, Wormholt Park, Wormholt Estate, World's End Estate, Woolwich Common and Royal Artillery Barracks', Woolwich Cemetery (Old and New), Wool House Garden, Woodside Park'
     print(y.replace("'",""))
 
 def main():
