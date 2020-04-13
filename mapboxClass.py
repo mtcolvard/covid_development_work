d26a1cbc8c966ba3c5cb2ce09ae69503062f05d6
mtcolvard <matthew.colvard@gmail.com>
2020-04-13 19:05:03 +0200

    while function for NxN matrix stuck in strange loop.  reverting back to Nx1 function for now for mvp

--- mapboxClass.py
+++ mapboxClass.py
@@ -13,14 +13,6 @@ t= [-0.045302810969366, 51.525117839052]
 y=[-0.055348551291013, 51.526184094351]
 u=[-0.075555186872678, 51.525618133595]
 i=[-0.017776262243614, 51.528252888321]
-q=[-0.048721755995117, 51.51258512161]
-w=[-0.045418257473052, 51.522421981895]
-e=[-0.01279619151928, 51.510183314858]
-r =[-0.068770393713962, 51.515614772134]
-t= [-0.045302810969366, 51.525117839052]
-y=[-0.055348551291013, 51.526184094351]
-u=[-0.075555186872678, 51.525618133595]
-i=[-0.017776262243614, 51.528252888321]
 o=[-0.061301863303676, 51.521786360354]
 p=[-0.013532569137843, 51.526382561487]
 a=[-0.05720990921333, 51.516323089494]
@@ -39,33 +31,29 @@ v=[-0.056712637941541, 51.528005220953]
 b=[-0.017116408507303, 51.510256445333]
 n=[-0.041096815873977, 51.522349881668]
 m=[-0.032918650586594, 51.511421844157]
-qq=[-0.03957915044187, 51.524123045013]
-ww=[-0.04541825747305, 51.522421981895]
-ee=[-0.041212499269476, 51.519654029999]
-rr=[-0.03147852577133, 51.511397678601]
-tt=[-0.042806996167133, 51.516083604578]
-yy=[-0.060395909406252, 51.509181744854]
-uu=[-0.041597980712523, 51.510667847417]
-ii=[-0.062064781654964, 51.503813771304]
-oo=[-0.013454340529617, 51.528179764083]
-pp=[-0.064335241794335, 51.518239429505]
-aa=[-0.017155426711002, 51.509357839359]
-ss=[-0.053195540296469, 51.509062430618]
-dd=[-0.065280496813107, 51.529945380685]
-ff=[-0.044208783987261, 51.517006252163]
-gg=[-0.048452902742563, 51.518875475744]
-hh=[-0.047050971377665, 51.517952877664]
+q,w,e,r,t,y,u,i,o,p,a,s,d,f,g,h,j,k,l,m,
+
+qq=[51.514353, -0.074898][::-1]
+ww=[51.519103, -0.060526][::-1]
+ee=[51.521890, -0.046287][::-1]
+rr=[51.525327, -0.033322][::-1]
+tt=[51.527436, -0.023635][::-1]
+yy=[51.531020, -0.011012][::-1]
+uu=[51.528275, 0.005575][::-1]
+ii=[51.531695, 0.018645][::-1]
+oo=[51.535669, 0.036456][::-1]
+pp=[51.525364, 0.059439][::-1]
 
 
 pride_of_spitalfields = (-0.071132, 51.518891)
-origin = (-0.071880,51.516319)
-destination = (-0.020552, 51.523678)
+origin = [51.513686, -0.088323][::-1]
+destination = [51.550540, 0.101267][::-1]
 list_of_waypoints = []
 
 
 service = DirectionsMatrix(access_token='pk.eyJ1IjoibXRjb2x2YXJkIiwiYSI6ImNrMDgzYndkZjBoanUzb21jaTkzajZjNWEifQ.ocEzAm8Y7a6im_FVc92HjQ')
 
-response = service.matrix([origin, destination, q,w,e,r,t,y,u,i,o,p,a,s,d,f,g,h,j,k,l,m,qq,rr], profile='mapbox/walking',  annotations=['distance'])
+response = service.matrix([origin, destination, qq,ww,ee,rr,tt,yy,uu,ii,oo,pp], profile='mapbox/walking',  annotations=['distance'])
 
 
 data = response.json()
@@ -76,20 +64,22 @@ def find_route_waypoints():
     # calculate the distance to each possible waypoint from both the origin and the destination
     number_of_loops = 0
     distances_from_origin = data['distances'][0]
+    # print(distances_from_origin)
+    distances_from_destination = data['distances'][1]
     while number_of_loops < 7:
-        distances_from_destination = data['distances'][1]
 
         # for each possible waypoint, sum its distance from both the origin and the destination and then find the waypoint with the smallest total distance.  that will the waypoint of the shortest route. use the index of that waypoint to get the lon_lat
         sum_distances = list(map(add, distances_from_origin, distances_from_destination))
         shortest_distance_index = sum_distances.index(sorted(sum_distances)[2])
         distances_from_origin = data['distances'][shortest_distance_index]
+        distances_from_origin.remove(distances_from_origin[shortest_distance_index])
+        # print(data['distances'][shortest_distance_index])
         number_of_loops = number_of_loops + 1
         waypoint_lon_lat = data['destinations'][shortest_distance_index]['location']
 
         list_of_waypoints.append(waypoint_lon_lat)
-        # print(sum_distances)
         # print(shortest_distance_index)
-        print(distances_from_origin)
+        # print(distances_from_origin)
         # print(waypoint_lon_lat)
         print(list_of_waypoints)
 
