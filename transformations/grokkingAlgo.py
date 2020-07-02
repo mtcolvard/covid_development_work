5437810c94031a664eb03237e8a14a0d8a6dbc78
mtcolvard <matthew.colvard@gmail.com>
2020-07-02 03:18:10 +0200

    algo and recursion practice. dijkstra's algorithm

--- transformations/grokkingAlgo.py
+++ transformations/grokkingAlgo.py
@@ -1,36 +1,88 @@
 from collections import deque
 graph = {}
-graph["you"] = ["alice", "bob", "claire"]
-graph["bob"] = ["anuj", "peggy"]
-graph["alice"] = ["peggy"]
-graph["claire"] = ["thom", "jonny"]
-graph["anuj"] = []
-graph["peggy"] = []
-graph["thom"] = []
-graph["jonny"] = []
+graph["start"] = {}
+graph["start"]["a"] = 6
+graph["start"]["b"] = 2
 
-def person_is_seller(name):
-    return name[-1] == 'm'
+graph['a'] = {}
+graph['a']['fin'] = 1
 
-def search(name):
-    print(graph)
-    search_que = deque()
-    search_que += graph[name]
-    searched = []
-    while search_que:
-        person = search_que.popleft()
-        if not person in searched:
-            if person_is_seller(person):
-                print(person + ' is mango seller')
-                return True
-            else:
-                search_que += graph[person]
-                searched.append(person)
-    return False
+graph['b'] = {}
+graph['b']['a'] = 3
+graph['b']['fin'] = 5
+
+graph['fin'] = {}
+
+infinity = float("inf")
+costs = {}
+costs["a"] = 6
+costs["b"] = 2
+costs["fin"] = infinity
+
+parents = {}
+parents["a"] = "start"
+parents["b"] = "start"
+parents["fin"] = None
+
+processed = []
+
+def find_lowest_cost_node():
+    lowest_cost = float('inf')
+    lowest_cost_node = None
+    for node in costs:
+        cost = costs[node]
+        if cost < lowest_cost and node not in processed:
+            lowest_cost = cost
+            lowest_cost_node = node
+    return lowest_cost_node
+
+def dijkstra():
+    node = find_lowest_cost_node()
+    while node is not None:
+        cost = costs[node]
+        neighbors = graph[node]
+        for n in neighbors.keys():
+            new_cost = cost + neighbors[n]
+            if costs[n] > new_cost:
+                costs[n] = new_cost
+                parents[n] = node
+        processed.append(node)
+        node = find_lowest_cost_node()
+    print(costs['fin'])
+    return costs['fin']
+# graph = {}
+# graph["you"] = ["alice", "bob", "claire"]
+# graph["bob"] = ["anuj", "peggy"]
+# graph["alice"] = ["peggy"]
+# graph["claire"] = ["thom", "jonny"]
+# graph["anuj"] = []
+# graph["peggy"] = []
+# graph["thom"] = []
+# graph["jonny"] = []
+#
+# def person_is_seller(name):
+#     return name[-1] == 'm'
+#
+# def search(name):
+#     print(graph)
+#     search_que = deque()
+#     search_que += graph[name]
+#     searched = []
+#     while search_que:
+#         person = search_que.popleft()
+#         if not person in searched:
+#             if person_is_seller(person):
+#                 print(person + ' is mango seller')
+#                 return True
+#             else:
+#                 search_que += graph[person]
+#                 searched.append(person)
+#     return False
 
 
 def main():
-    search('you')
+    # search('you')
+    dijkstra()
 
 
 
